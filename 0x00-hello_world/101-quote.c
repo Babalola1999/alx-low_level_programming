e8068775174e61c4f865c880c782b19208705c1f
Babalola1999 <sojitimilehin@gmail.com>
2023-09-11 18:24:17 -0700

    0-9

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A C PROGRAM THAT PRINT A LINE TO THE STANDARD ERROR
+ *
+ * Return: 1(successful)
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
