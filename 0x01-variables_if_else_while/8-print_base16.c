222d1ac93d4d55ac71656cbf8ca1d4b27e7894f5
Babalola1999 <sojitimilehin@gmail.com>
2023-09-11 20:05:27 -0700

    u

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main fuction
+ *
+ * Return: zero
+ */
+
+int main(void)
+{
+	int i;
+	char j;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	for (j = 0 ; j <= 'f' ; j++);
+	putchar(j);
+		putchar('\n');
+		return (0);
+}
