9ea786f73ec437fa3bf2b5ef129db4620fb06405
Babalola1999 <sojitimilehin@gmail.com>
2023-09-15 03:05:12 -0700

    akphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,12 +3,12 @@
  * main - check code
  *
  * Return: always 0
- */
-void print_alphabet(void);
+*/
+void print_alphabet(void)
 {
-	int i;
+	char i;
 
 	for (i = 'a' ; i <= 'z' ; i++)
 		_putchar(i);
-	_putchar('\n')
+	_putchar('\n');
 }
